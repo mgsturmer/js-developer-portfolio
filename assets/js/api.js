623e8e3c217ef6bba452d35f306122ad7bb6f2e6
Matheus Sturmer <mgsturmer@gmail.com>
2025-03-24 13:20:19 -0300

    Atualizando portfólio com meus dados

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,11 +1,7 @@
 
 async function fetchProfileData() {
-    const url = './'
-    /profile.json';
+    const url = 'https://raw.githubusercontent.com/mgsturmer/js-developer-portfolio/refs/heads/main/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
 }
-
-
-'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
