62f1d9825ed7d1666f0ce4a0a8098e24ce2de7a2
Matheus Sturmer <mgsturmer@gmail.com>
2025-03-24 14:14:28 -0300

    Mais correções e atualizações em documento profile.json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/mgsturmer/js-developer-portfolio/refs/heads/main/data/profile.json'
+const url = 'https://raw.githubusercontent.com/mgsturmer/js-developer-portfolio/refs/heads/main/data/profile.json'
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
